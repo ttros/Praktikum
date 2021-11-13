76074bdf9eeee1e3f80306d58e140e6ca65923fa
Tom Troska <tom.troska@tu-dortmund.de>
2021-11-13 19:21:10 +0100

    Ordner fuer Protokolle hinzugefuegt. Hier koennen wir fuer jeden Versuch einen neuen Unterordner vom Typ vXXX erstellen.

---
+++ Protokolle/v353/plot.py
@@ -0,0 +1,22 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+x = np.linspace(0, 10, 1000)
+y1 = np.sin(x)
+y2 = np.cos(x)
+
+plt.subplot(1, 2, 1)
+plt.plot(x, y1, label='sin')
+plt.xlabel(r'$\alpha \mathbin{/} \unit{\ohm}$')
+plt.ylabel(r'$y \mathbin{/} \unit{\micro\joule}$')
+plt.legend(loc='best')
+
+plt.subplot(1, 2, 2)
+plt.plot(x, y2, label='cos')
+plt.xlabel(r'$\alpha \mathbin{/} \unit{\ohm}$')
+plt.ylabel(r'$y \mathbin{/} \unit{\micro\joule}$')
+plt.legend(loc='best')
+
+# in matplotlibrc leider (noch) nicht möglich
+plt.tight_layout(pad=0, h_pad=1.08, w_pad=1.08)
+plt.savefig('build/plot.pdf')
