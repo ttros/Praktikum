cf2172b070d9ae26348f4c8edfcf5f852d855f0a
Tom Troska <tom.troska@tu-dortmund.de>
2021-11-13 20:31:07 +0100

    Aenderung des Zitatstyles in der Vorlage

--- Protokolle/v353/plot.py
+++ Protokolle/v353/plot.py
@@ -2,17 +2,16 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 x = np.linspace(0, 10, 1000)
-y1 = np.sin(x)
-y2 = np.cos(x)
+y = x ** np.sin(x)
 
 plt.subplot(1, 2, 1)
-plt.plot(x, y1, label='sin')
+plt.plot(x, y, label='Kurve')
 plt.xlabel(r'$\alpha \mathbin{/} \unit{\ohm}$')
 plt.ylabel(r'$y \mathbin{/} \unit{\micro\joule}$')
 plt.legend(loc='best')
 
 plt.subplot(1, 2, 2)
-plt.plot(x, y2, label='cos')
+plt.plot(x, y, label='Kurve')
 plt.xlabel(r'$\alpha \mathbin{/} \unit{\ohm}$')
 plt.ylabel(r'$y \mathbin{/} \unit{\micro\joule}$')
 plt.legend(loc='best')
