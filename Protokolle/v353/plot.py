9d81a9d7c34ae786018269df5ba6dcda918dd6b8
Theodor Zies <theodor.zies@tu-dortmund.de>
2021-11-21 13:07:36 +0100

    Fehler

--- Protokolle/v353/plot.py
+++ Protokolle/v353/plot.py
@@ -17,13 +17,13 @@ B=unp.uarray(b,std)
 
 plt.plot(t, m*t+b, 'b', label='Fit')
 plt.annotate(f'$ln(U) =  {m} \cdot t + {b}$', [0.8,0.2])
-plt.errorbar(t, np.log(u), xerr = stds(T), yerr = stds(U), fmt = 'rx') #Fehler y anpassen!!! Gaussssss
+plt.errorbar(t, np.log(u), xerr = stds(T), yerr = stds(U), fmt = "r.", label='Daten')
 #plt.plot(t, np.log(U), 'rx', label='Daten')
 plt.xlabel(r'$t$ [$\symup{\mu}$s]')
 plt.ylabel(r'$ln(U)$ [V]')
 plt.legend(loc='best')
 
-print(f'RC_a = {1/M} [\mu s]')
+print(f'RC_a = {-1/M} [\mu s]')
 print(f'Steigung m: {M}')
 print(f'Achsenabschnitt b: {B}')
 
@@ -33,14 +33,19 @@ plt.close()
 
 ## Beginn Plot B
 f, A, a, b = np.genfromtxt('content/data_bc.txt', unpack = True)
-phi = a/b * 2 * np.pi
+
+F_f = unp.uarray(f,0.1) #Fehler von f
+A_f = unp.uarray(A,0.1) #Fehler von A
 
 def f1(f,c):
     return 1/(np.sqrt(1+(f**2 * c**2)))
 
-parameters, pcov = curve_fit(f1, f, A/2.8, sigma=None)
-print(f'RC_b = {parameters} [s]')
-plt.plot(np.log(f), A/2.8, 'rx', label='Daten')
+parameters, pcov = curve_fit(f1, f , A/2.8, sigma=None)
+RC_B=unp.uarray(parameters,pcov)
+#print(f'RC_b = {parameters*10**6} [\mu s]')
+print(f'RC_b = {RC_B*10**6} [\mu s]')
+#plt.plot(np.log(f), A/2.8, 'rx', label='Daten')
+plt.errorbar(np.log(f), A/2.8 , xerr = stds(F_f), yerr = stds(A_f), fmt = 'r.', label='Daten')
 plt.plot(np.log(f), f1(f,*parameters), 'b', label='Fit')
 
 plt.xlabel(r'$f$ [Hz]')
@@ -52,12 +57,20 @@ plt.savefig('build/plot_b.pdf')
 plt.close()
 
 ## Beginn Plot C
+
+a_f = unp.uarray(a,0.1) #Fehler von a
+b_f = unp.uarray(b,0.1) #Fehler von b
+
+phi = a_f/b_f * 2 * np.pi
+
 def f2(f,c):
     return np.arctan(-f*c)
 
-parameters, pcov = curve_fit(f2, f, phi, sigma=None)
-print(f'RC_c = {parameters} [s]')
-plt.plot(np.log(f), phi, 'rx', label='Daten')
+parameters, pcov = curve_fit(f2, f, noms(phi), sigma=None)
+RC_C=unp.uarray(parameters,pcov)
+print(f'RC_c = {-RC_C*10**6} [\mu s]')
+#plt.plot(np.log(f), phi, 'rx', label='Daten')
+plt.errorbar(np.log(f), noms(phi) , xerr = stds(F_f), yerr = stds(phi), fmt = 'r.', label='Daten')
 plt.plot(np.log(f), f2(f,*parameters), 'b', label='Fit')
 
 plt.xlabel(r'$log(f)$ [Hz]')
@@ -71,11 +84,10 @@ plt.close()
 ## Beginn Plot D
 phi2 = np.linspace(0, (np.pi)/2, 50)
 
-plt.polar(phi, A, 'rx')
+plt.polar(noms(phi), A, 'rx')
 #plt.polar(phi2, np.sin(phi2)/(np.tan(phi2))*2.8, 'b')
-plt.polar(phi2, np.cos(phi2)*2.8, 'b')
+plt.polar(phi2, np.cos(phi2)*2.8, fmt = 'b')
 
 plt.tight_layout(pad=0, h_pad=1.08, w_pad=1.08)
 plt.savefig('build/plot_d.pdf')
-plt.close() 
-
+plt.close()
