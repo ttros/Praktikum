04ebaa9683e9ad4690090e03907b3ea862021802
Theodor Zies <theodor.zies@tu-dortmund.de>
2021-12-04 15:15:21 +0100

    meer auswertun

--- Protokolle/v103/plot.py
+++ Protokolle/v103/plot.py
@@ -11,15 +11,17 @@ from uncertainties.unumpy import (nominal_values as noms,
 #runder Stab einseitig
 
 X_ohne_Fehler, D_0_rund_ohne_Fehler, D_G_rund_ohne_Fehler= np.genfromtxt('content/Daten/einseitig_rund.txt', unpack = True)
-X = unp.uarray(X_ohne_Fehler, 0.01)
+X = unp.uarray(X_ohne_Fehler, 0.1)
+L = 48
+Eta = L*X**2-(1/3)*X**3
 D_0_rund = unp.uarray(D_0_rund_ohne_Fehler, 0.01)
 D_G_rund = unp.uarray(D_G_rund_ohne_Fehler, 0.01)
 
-D_X = (D_G_rund-D_0_rund)
+D_X = (D_0_rund-D_G_rund)
 
-plt.errorbar(noms(X), noms(D_X) , xerr = stds(X), yerr = stds(D_X), fmt = 'r.', label='Daten')
-plt.xlabel(r'$X$ [$\unit{\centi\meter}$]')
-plt.ylabel(r'$D(X)$ [$\unit{\milli\meter}$]')
+plt.errorbar(noms(Eta), noms(D_X) , xerr = stds(Eta), yerr = stds(D_X), fmt = 'r.', label='Daten')
+plt.xlabel(r'$\eta(x)$ [$\unit{\centi\meter}$]')
+plt.ylabel(r'$\Delta D(x)$ [$\unit{\milli\meter}$]')
 plt.legend(loc='best')
 
 plt.savefig('build/plot_rund_einseitig.pdf')
@@ -28,15 +30,15 @@ plt.close()
 #runder Stab beidseitig
 
 X_ohne_Fehler, D_0_rund_ohne_Fehler, D_G_rund_ohne_Fehler= np.genfromtxt('content/Daten/beidseitig_rund.txt', unpack = True)
-X = unp.uarray(X_ohne_Fehler, 0.01)
+X = unp.uarray(X_ohne_Fehler, 0.1)
 D_0_rund = unp.uarray(D_0_rund_ohne_Fehler, 0.01)
 D_G_rund = unp.uarray(D_G_rund_ohne_Fehler, 0.01)
 
-D_X = (D_G_rund-D_0_rund)
+D_X = (D_0_rund-D_G_rund)
 
 plt.errorbar(noms(X), noms(D_X) , xerr = stds(X), yerr = stds(D_X), fmt = 'r.', label='Daten')
-plt.xlabel(r'$X$ [$\unit{\centi\meter}$]')
-plt.ylabel(r'$D(X)$ [$\unit{\milli\meter}$]')
+plt.xlabel(r'$x$ [$\unit{\centi\meter}$]')
+plt.ylabel(r'$\Delta D(x)$ [$\unit{\milli\meter}$]')
 plt.legend(loc='best')
 
 plt.savefig('build/plot_rund_beidseitig.pdf')
@@ -45,15 +47,17 @@ plt.close()
 #eckiger Stab einseitig
 
 X_ohne_Fehler, D_0_eckig_ohne_Fehler, D_G_eckig_ohne_Fehler= np.genfromtxt('content/Daten/einseitig_eckig.txt', unpack = True)
-X = unp.uarray(X_ohne_Fehler, 0.01)
+X = unp.uarray(X_ohne_Fehler, 0.1)
+L = 48
+Eta = L*X**2-(1/3)*X**3
 D_0_eckig = unp.uarray(D_0_eckig_ohne_Fehler, 0.01)
 D_G_eckig = unp.uarray(D_G_eckig_ohne_Fehler, 0.01)
 
-D_X = (D_G_eckig-D_0_eckig)
+D_X = (D_0_eckig-D_G_eckig)
 
-plt.errorbar(noms(X), noms(D_X) , xerr = stds(X), yerr = stds(D_X), fmt = 'r.', label='Daten')
-plt.xlabel(r'$X$ [$\unit{\centi\meter}$]')
-plt.ylabel(r'$D(X)$ [$\unit{\milli\meter}$]')
+plt.errorbar(noms(Eta), noms(D_X) , xerr = stds(Eta), yerr = stds(D_X), fmt = 'r.', label='Daten')
+plt.xlabel(r'$\eta(x)$ [$\unit{\centi\meter}$]')
+plt.ylabel(r'$\Delta D(x)$ [$\unit{\milli\meter}$]')
 plt.legend(loc='best')
 
 plt.savefig('build/plot_eckig_einseitig.pdf')
@@ -62,15 +66,15 @@ plt.close()
 #eckiger Stab beidseitig
 
 X_ohne_Fehler, D_0_eckig_ohne_Fehler, D_G_eckig_ohne_Fehler= np.genfromtxt('content/Daten/beidseitig_eckig.txt', unpack = True)
-X = unp.uarray(X_ohne_Fehler, 0.01)
+X = unp.uarray(X_ohne_Fehler, 0.1)
 D_0_eckig = unp.uarray(D_0_eckig_ohne_Fehler, 0.01)
 D_G_eckig = unp.uarray(D_G_eckig_ohne_Fehler, 0.01)
 
-D_X = (D_G_eckig-D_0_eckig)
+D_X = (D_0_eckig-D_G_eckig)
 
 plt.errorbar(noms(X), noms(D_X) , xerr = stds(X), yerr = stds(D_X), fmt = 'r.', label='Daten')
-plt.xlabel(r'$X$ [$\unit{\centi\meter}$]')
-plt.ylabel(r'$D(X)$ [$\unit{\milli\meter}$]')
+plt.xlabel(r'$x$ [$\unit{\centi\meter}$]')
+plt.ylabel(r'$\Delta D(x)$ [$\unit{\milli\meter}$]')
 plt.legend(loc='best')
 
 plt.savefig('build/plot_eckig_beidseitig.pdf')
