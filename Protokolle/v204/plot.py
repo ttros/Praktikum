9001f98c2e2e5ad7278baa85ad4ad812339e1d2b
Tom Troska <tom.troska@tu-dortmund.de>
2021-12-07 18:32:46 +0100

    mini Aenderungen

--- Protokolle/v204/plot.py
+++ Protokolle/v204/plot.py
@@ -1,21 +1,45 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import uncertainties as unp
+from scipy import stats
+import uncertainties.unumpy as unp
+from uncertainties import ufloat
+from uncertainties.unumpy import (nominal_values as noms,
+                                  std_devs as stds)
 
-x = np.linspace(0, 10, 1000)
-y = x ** np.sin(x)
+######
+# statisch
 
-plt.subplot(1, 2, 1)
-plt.plot(x, y, label='Kurve')
-plt.xlabel(r'$\alpha \mathbin{/} \unit{\ohm}$')
-plt.ylabel(r'$y \mathbin{/} \unit{\micro\joule}$')
-plt.legend(loc='best')
+t_roh, T_1_roh, T_2_roh, T_3_roh, T_4_roh, T_5_roh, T_6_roh, T_7_roh, T_8_roh = np.genfromtxt('content/data/data_statisch.txt', unpack = True)
 
-plt.subplot(1, 2, 2)
-plt.plot(x, y, label='Kurve')
-plt.xlabel(r'$\alpha \mathbin{/} \unit{\ohm}$')
-plt.ylabel(r'$y \mathbin{/} \unit{\micro\joule}$')
-plt.legend(loc='best')
+plt.plot(t_roh, T_1_roh)
 
-# in matplotlibrc leider (noch) nicht möglich
+
+#plt.plot(X,m_rund_einseitig*X+b_rund_einseitig ,'b', label = 'Fit')
+#plt.errorbar(noms(Eta), noms(D_X) , xerr = stds(Eta), yerr = stds(D_X), fmt = 'r.', label='Daten')
+#plt.xlabel(r'$\eta(x)$ [$\unit{\cubic\meter}$]')
+#plt.ylabel(r'$D(x)$ [$\unit{\meter}$]')
+#plt.legend(loc='best')
+
 plt.tight_layout(pad=0, h_pad=1.08, w_pad=1.08)
-plt.savefig('build/plot.pdf')
+plt.savefig('build/plot_statisch.pdf')
+plt.close()
+
+######
+# dynamisch, kurz
+t_roh, T_1_roh, T_2_roh, T_3_roh, T_4_roh, T_5_roh, T_6_roh, T_7_roh, T_8_roh = np.genfromtxt('content/data/data_dynamisch_kurz.txt', unpack = True)
+
+plt.plot(t_roh, T_1_roh)
+plt.tight_layout(pad=0, h_pad=1.08, w_pad=1.08)
+plt.savefig('build/plot_dynamisch_kurz.pdf')
+plt.close()
+######
+
+######
+t_roh, T_1_roh, T_2_roh, T_3_roh, T_4_roh, T_5_roh, T_6_roh, T_7_roh, T_8_roh = np.genfromtxt('content/data/data_dynamisch_lang.txt', unpack = True)
+
+plt.plot(t_roh, T_1_roh)
+plt.tight_layout(pad=0, h_pad=1.08, w_pad=1.08)
+plt.savefig('build/plot_dynamisch_lang.pdf')
+plt.close()
+######
