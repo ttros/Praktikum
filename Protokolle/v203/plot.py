6ae7d25df6d08a8110dd75200e0138d2f0412cb1
Tom Troska <tom.troska@tu-dortmund.de>
2022-01-12 13:53:38 +0100

    Plots komisch

--- Protokolle/v203/plot.py
+++ Protokolle/v203/plot.py
@@ -37,6 +37,7 @@ plt.grid(which="both")
 plt.tight_layout(pad=0, h_pad=1.08, w_pad=1.08)
 plt.savefig('build/plot_1.pdf')
 plt.close()
+
 ############### Plot 1 ################
 ############ Berechnung L #############
 L = -M*8.314
@@ -95,4 +96,40 @@ print(f'C: {C}')
 print(f'D: {D}')
 print(f'\n\n')
 ############## Ausgabe 2 ##############
+############## Plot 3,4 ###############
+a = noms(A)
+b = noms(B)
+c = noms(C)
+d = noms(D)
+a_tilde = 0.9 # Jm^3/mol^2 aus Aufgabenstellung
+R = 8.314 # Gaskonstante
+
+def L_minus(T,a,b,c,d,a_tilde,R):
+    return ((T*(3*a*T**2 + 2*b*T + c)) / (a*T**3 + b*T**2 + c*T + d)) * (((R*T)/2) - np.sqrt((R**2 * T**2)/4 - a_tilde*10**3*(a*T**3 + b*T**2 + c*T + d)))
+
+def L_plus(T,a,b,c,d,a_tilde,R):
+    return ((T*(3*a*T**2 + 2*b*T + c)) / (a*T**3 + b*T**2 + c*T + d)) * (((R*T)/2) + np.sqrt((R**2 * T**2)/4 - a_tilde*10**3*(a*T**3 + b*T**2 + c*T + d)))
+
+xx = np.linspace(370, 480, 1000)
+
+plt.plot(xx, L_minus(xx,a,b,c,d,a_tilde,R), 'b')
+
+plt.xlabel(r'$T \,/\,\unit{\kelvin}$')
+plt.ylabel(r'$L_{-}(T) \,/\, \unit{\joule\per\mol}$')
+plt.grid(which="both")
+
+plt.tight_layout(pad=0, h_pad=1.08, w_pad=1.08)
+plt.savefig('build/plot_L_minus.pdf')
+plt.close()
+
+plt.plot(xx, L_plus(xx,a,b,c,d,a_tilde,R), 'b')
+
+plt.xlabel(r'$T \,/\,\unit{\kelvin}$')
+plt.ylabel(r'$L_{-}(T) \,/\, \unit{\joule\per\mol}$')
+plt.grid(which="both")
+
+plt.tight_layout(pad=0, h_pad=1.08, w_pad=1.08)
+plt.savefig('build/plot_L_plus.pdf')
+plt.close()
+############## Plot 3,4 ###############
 # %%%%%%%%%%%%%%%%%%%%%% TEIL 2 %%%%%%%%%%%%%%%%%%%%%% #
