970d66833cc275b1a739b250b2e33f1f542e1e21
Tom Troska <tom.troska@tu-dortmund.de>
2022-04-05 19:50:30 +0200

    Abbildungen und Tabellen

--- Protokolle/US1/plot.py
+++ Protokolle/US1/plot.py
@@ -1,21 +1,56 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-x = np.linspace(0, 10, 1000)
-y = x ** np.sin(x)
+import uncertainties as unp
+from scipy import stats
+from scipy.optimize import curve_fit
+import uncertainties.unumpy as unp
+from uncertainties import ufloat
+from uncertainties.unumpy import (nominal_values as noms,
+                                  std_devs as stds)
+import scipy.constants as const
 
-plt.subplot(1, 2, 1)
-plt.plot(x, y, label='Kurve')
-plt.xlabel(r'$\alpha \mathbin{/} \unit{\ohm}$')
-plt.ylabel(r'$y \mathbin{/} \unit{\micro\joule}$')
+# %%%%% c-Bestimmung I-E-Verfahren %%%%%
+l, t_ = np.genfromtxt('content/data/c-Bestimmung_IE.txt', unpack = True)
+t = t_/2
+print(t[-2])
+plt.plot(l,t,'rx',label='Messwerte')
+
+plt.xlabel(r'$l \,/\,\unit{\milli\metre}')
+plt.ylabel(r'$\symup{\Delta}t$ \,/\, \unit{\micro\second}')
 plt.legend(loc='best')
+plt.grid(which="both")
 
-plt.subplot(1, 2, 2)
-plt.plot(x, y, label='Kurve')
-plt.xlabel(r'$\alpha \mathbin{/} \unit{\ohm}$')
-plt.ylabel(r'$y \mathbin{/} \unit{\micro\joule}$')
-plt.legend(loc='best')
-
-# in matplotlibrc leider (noch) nicht möglich
 plt.tight_layout(pad=0, h_pad=1.08, w_pad=1.08)
-plt.savefig('build/plot.pdf')
+plt.savefig('build/c-Bestimmung_IE.pdf')
+plt.close()
+
+# %%%%% c-Bestimmung D-Verfahren %%%%%
+l, t = np.genfromtxt('content/data/c-Bestimmung_D.txt', unpack = True)
+plt.plot(l,t,'rx',label='Messwerte')
+
+plt.xlabel(r'$l \,/\,\unit{\milli\metre}')
+plt.ylabel(r'$\symup{\Delta}t$ \,/\, \unit{\micro\second}')
+plt.legend(loc='best')
+plt.grid(which="both")
+
+plt.tight_layout(pad=0, h_pad=1.08, w_pad=1.08)
+plt.savefig('build/c-Bestimmung_D.pdf')
+plt.close()
+
+# %%%%% Daempfungsbestimmung IE-Verfahren %%%%%
+l, A_out, A_in, output = np.genfromtxt('content/data/Daempfung_IE.txt', unpack = True)
+
+A = A_in/A_out
+
+plt.plot(l,np.log(A),'rx',label='Messwerte') #Achsenbeschriftung anpassen!
+
+
+plt.xlabel(r'$l \,/\,\unit{\milli\metre}')
+plt.ylabel(r'$\symup{\Delta}t$ \,/\, \unit{\micro\second}')
+plt.legend(loc='best')
+plt.grid(which="both")
+
+plt.tight_layout(pad=0, h_pad=1.08, w_pad=1.08)
+plt.savefig('build/Daempfungsbestimmung.pdf')
+plt.close()
