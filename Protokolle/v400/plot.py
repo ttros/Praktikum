50c741c86c2036467e62bb746c346638e4e642eb
Theodor Zies <theodor.zies@tu-dortmund.de>
2022-05-18 18:32:53 +0200

    schonmal n start

--- Protokolle/v400/plot.py
+++ Protokolle/v400/plot.py
@@ -10,3 +10,16 @@ from uncertainties.unumpy import (nominal_values as noms,
                                   std_devs as stds)
 import scipy.constants as const
 import scipy.optimize as op
+
+#Aufgabe 2#
+
+alpha_deg = np.array([10,20,30,40,50,60,70])
+alpha_rad = ((2*np.pi)/(360))*alpha_deg
+beta_deg = np.array([6.5,13,19.5,25.5,31,35.5,39])
+beta_rad = ((2*np.pi)/(360))*beta_deg
+
+n_array = np.sin(alpha_rad)/np.sin(beta_rad)
+n = ufloat(np.mean(n_array),np.std(n_array))
+print(n)
+v=2.99793*10**6/n
+print(v)
