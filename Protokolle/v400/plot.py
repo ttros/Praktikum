11f80b524e286a4e09b5d4db39d936aa87e07bf2
Theodor Zies <theodor.zies@tu-dortmund.de>
2022-05-19 13:57:39 +0200

    Hälfte vertik düt

--- Protokolle/v400/plot.py
+++ Protokolle/v400/plot.py
@@ -13,13 +13,20 @@ import scipy.optimize as op
 
 #Aufgabe 2#
 
-alpha_deg = np.array([10,20,30,40,50,60,70])
+alpha_deg = np.array([10.0,20.0,30.0,40.0,50.0,60.0,70.0])
 alpha_rad = ((2*np.pi)/(360))*alpha_deg
-beta_deg = np.array([6.5,13,19.5,25.5,31,35.5,39])
+beta_deg = np.array([6.5,13.0,19.5,25.5,31.0,35.5,39.0])
 beta_rad = ((2*np.pi)/(360))*beta_deg
 
 n_array = np.sin(alpha_rad)/np.sin(beta_rad)
 n = ufloat(np.mean(n_array),np.std(n_array))
-print(n)
+print(f'Brechungsindex: {n}')
 v=2.99793*10**6/n
-print(v)
+print(f'c in Plexiglas: {v}')
+
+d=5.85
+s_methode1=d*(np.sin(alpha_rad-beta_rad))/(np.cos(beta_rad))
+print(f'Strahlenversatz 1.Methode: {s_methode1}')
+beta_calc=np.arcsin(np.sin(alpha_rad)/np.mean(n_array))
+s_methode2=d*(np.sin(alpha_rad-beta_calc))/(np.cos(beta_calc))
+print(f'Strahlenversatz 2.Methode: {s_methode2}')
