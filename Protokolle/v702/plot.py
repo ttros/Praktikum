800ed55d6fbf7a358f9ded2c8784e1563c706c75
Tom Troska <tom.troska@tu-dortmund.de>
2022-05-31 15:00:32 +0200

    los gehts

---
+++ Protokolle/v702/plot.py
@@ -0,0 +1,8 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import uncertainties as unc
+from uncertainties import unumpy as unp 
+from uncertainties import ufloat
+from uncertainties.unumpy import nominal_values as noms
+from uncertainties.unumpy import std_devs as devs
+
