258cbe1f176b225971a1e7c6c753004f5c030155
Tom Troska <tom.troska@tu-dortmund.de>
2022-06-09 17:40:15 +0200

    auswertung weiter

--- Protokolle/v704/plot.py
+++ Protokolle/v704/plot.py
@@ -41,14 +41,14 @@ N_beta_log = unp.log(N_beta)
 
 # cut parameter
 cut_beta = 6
-d_max = 252
 
 # Plot
 plt.errorbar(noms(d_beta), noms(N_beta_log), linestyle = None, fmt='.', xerr = stds(d_beta), yerr = stds(N_beta_log), c='tomato', label='Messwerte')
 M_beta, B_beta = regression(noms(d_beta[:cut_beta]),noms(N_beta_log[:cut_beta]),95,270,'dodgerblue','Fit') # Fit
+d_max = (unp.log(N_0_beta) - B_beta) / M_beta # d_max als schnittpunkt berechnen
 xx = np.linspace(95,485, 1000)
 plt.plot(xx, 0*xx+np.log(noms(N_0_beta)), c='slategray', label='Hintergrund')
-plt.vlines(d_max, ymin = -0.8, ymax = 3.8, color='darkolivegreen', linestyle = ':', 
+plt.vlines(noms(d_max), ymin = -0.8, ymax = 3.8, color='darkolivegreen', linestyle = ':', 
 label = r'$d_\text{max}=\qty{252}{\micro\metre}$')
 
 plt.xlabel(r'$d \mathbin{/} \unit{\micro\metre}$')
@@ -89,6 +89,7 @@ M_Zn, B_Zn = regression(noms(d_Zn),noms(N_Zn_log),1.5,20.5,'dodgerblue','Fit') #
 plt.xlabel(r'$d \mathbin{/} \unit{\milli\metre}$')
 plt.ylabel(r'$\ln(N_{\symup{S}})$')
 
+plt.title('Zink')
 plt.grid()
 plt.legend()
 plt.tight_layout(pad=0, h_pad=1.08, w_pad=1.08)
@@ -103,12 +104,20 @@ M_Pb, B_Pb = regression(noms(d_Pb),noms(N_Pb_log),-1,41,'dodgerblue','Fit') # Fi
 plt.xlabel(r'$d \mathbin{/} \unit{\milli\metre}$')
 plt.ylabel(r'$\ln(N_{\symup{S}})$')
 
+plt.title('Blei')
 plt.grid()
 plt.legend()
 plt.tight_layout(pad=0, h_pad=1.08, w_pad=1.08)
 
 plt.savefig('build/Pb.pdf')
 plt.close()
+
+# RECHNUNGEN
+r_max = 2.7 * d_max*10**-5
+E_max = 1.92 * unp.sqrt((r_max)**2+0.22*r_max)
+
+N_0_Zn = (np.e)**B_Zn
+N_0_Pb = (np.e)**B_Pb
 
 # PRINT
 print('############ Ausgabe V704 ############')
@@ -119,10 +128,33 @@ print(f'N_0_gamma \t',N_0_gamma)
 print(f'N_0_gamma_roh \t',N_0_gamma_unbereinigt)
 
 print('--------- Plot beta-Strahler ---------')
+print(f'M_beta: \t', M_beta)
+print(f'B_beta: \t', B_beta)
+print(f'd_max: \t', d_max)
+print(f'r_max: \t', r_max)
+print(f'E_max: \t',E_max)
+print('--------- Plot gamma-Strahler --------')
 print(f'M_Zn: \t', M_Zn)
 print(f'B_Zn: \t', B_Zn)
-
-print('--------- Plot gamma-Strahler --------')
 print(f'M_Pb: \t', M_Pb)
 print(f'B_Pb: \t', B_Pb)
+print(f'N_0_Zn: \t',N_0_Zn)
+print(f'N_0_Pb: \t',N_0_Pb)
 
+
+'''''
+############ Ausgabe V704 ############
+------------ Nullmessung -------------
+N_0_beta: 	 0.692+/-0.028
+N_0_beta_roh 	 623
+N_0_gamma 	 1.111+/-0.035
+N_0_gamma_roh 	 1000
+--------- Plot beta-Strahler ---------
+M_beta: 	 -0.0234+/-0.0026
+B_beta: 	 5.5+/-0.4
+--------- Plot gamma-Strahler --------
+M_Zn: 	 -0.0426+/-0.0014
+B_Zn: 	 4.719+/-0.017
+M_Pb: 	 -0.0929+/-0.0019
+B_Pb: 	 4.70+/-0.04
+'''''
