3a88f75c45abf9c654f55e32dff388d25841845c
Tom Troska <tom.troska@tu-dortmund.de>
2022-06-23 21:52:50 +0200

    auswertung weiter

--- Protokolle/v703/plot.py
+++ Protokolle/v703/plot.py
@@ -28,8 +28,8 @@ def regression(x,y,x1,x2,Farbe,Name):
 
 # Variablen
 messdauer = 120   # ggf. normieren, messdauer vermutlich 120s
-p_min = 3       # plateau definieren
-p_max = 7
+p_min = 6       # plateau definieren
+p_max = -2
 
 # Daten einlesen
 U, N_, I_= np.genfromtxt('content/data/data.txt', unpack = True)
@@ -37,13 +37,31 @@ U, N_, I_= np.genfromtxt('content/data/data.txt', unpack = True)
 N = unp.uarray(N_, np.sqrt(N_)) / messdauer
 I = unp.uarray(I_, 0.1)*10**(-6)
 
-plt.errorbar(U[:p_min], noms(N)[:p_min], yerr=stds(N)[:p_min], linestyle = None, fmt='.', c='grey', capsize=3, label='Messwerte')
-plt.errorbar(U[p_min:p_max], noms(N)[p_min:p_max], yerr=stds(N)[p_min:p_max], linestyle = None, fmt='.', c='indianred', capsize=3, label='Messwerte Plateaubereich')
-plt.errorbar(U[p_max:], noms(N)[p_max:], yerr=stds(N)[p_max:], linestyle = None, fmt='.', c='grey', capsize=3)
+# Plot roh
+dumme_werte = [10,11,12,13,14,20,21,22,23]
+U_mod = np.delete(U,dumme_werte)
+N_mod = np.delete(N,dumme_werte)
 
-#M, B = regression(U[p_min:p_max], noms(N)[p_min:p_max], U[p_min]-5, U[p_max-1]+5, 'dodgerblue', 'Fit')
-M =1
-B =1
+plt.errorbar(U, noms(N), yerr=stds(N), linestyle = None, fmt='.', c='darkorange', capsize=3, label='Messwerte (Ausreißer)')
+plt.errorbar(U_mod, noms(N_mod), yerr=stds(N_mod), linestyle = None, fmt='.', c='forestgreen', capsize=3, label='Messwerte')
+
+plt.xlabel(r'$U \mathbin{/} \unit{\volt}')
+plt.ylabel(r'$N \mathbin{/} \unit{\second}$')
+
+plt.grid()
+plt.legend()
+plt.tight_layout(pad=0, h_pad=1.08, w_pad=1.08)
+
+plt.savefig('build/charakteristik_roh.pdf')
+plt.close()
+
+#Plot fein, ohne dummen Werte
+plt.errorbar(U_mod[:p_min], noms(N_mod)[:p_min], yerr=stds(N_mod)[:p_min], linestyle = None, fmt='.', c='grey', capsize=3, label='Messwerte')
+plt.errorbar(U_mod[p_min:p_max], noms(N_mod)[p_min:p_max], yerr=stds(N_mod)[p_min:p_max], linestyle = None, fmt='.', c='indianred', capsize=3, label='Messwerte Plateaubereich')
+plt.errorbar(U_mod[p_max:], noms(N_mod)[p_max:], yerr=stds(N_mod)[p_max:], linestyle = None, fmt='.', c='grey', capsize=3)
+
+M, B = regression(U_mod[p_min:p_max], noms(N_mod)[p_min:p_max], U[p_min]-5, U[p_max-1]+5, 'dodgerblue', 'Fit')
+
 plt.xlabel(r'$U \mathbin{/} \unit{\volt}')
 plt.ylabel(r'$N \mathbin{/} \unit{\second}$')
 
@@ -100,3 +118,102 @@ print(f'*******************')
 for i in range(len(dQ)):
     print('{0:.3e}'.format(dQ[i]))
 print(f'*******************')
+print(f'*******************')
+print(f'N mit Fehler')
+for i in range(len(N)):
+    print('{0:.0f}'.format(N[i]*messdauer))
+print(f'******************')
+
+'''
+############ Ausgabe V703 ############
+----------- Charakteristik -----------
+lin. Bereich: 	 	 390.0 V bis 680.0 V
+Plateaulaenge: 	 	 290.0 V
+Steigung: 	 	 0.0070+/-0.0019 in ???
+y_Achsenabschnitt: 	 85.0+/-1.0 in ???
+--------------- Totzeit --------------
+Totzeit 2 Quellen Meth.: (5.542+/-0.029)e-05 in s
+-------------- Ladungen --------------
+freigesetzte Ladungen in eV
+*******************
+(7.581+/-7.581)e+09
+(1.499+/-0.750)e+10
+(1.466+/-0.733)e+10
+(1.475+/-0.737)e+10
+(1.441+/-0.721)e+10
+(1.451+/-0.725)e+10
+(1.434+/-0.717)e+10
+(2.137+/-0.713)e+10
+(2.149+/-0.717)e+10
+(2.795+/-0.699)e+10
+(2.184+/-0.546)e+10
+(1.829+/-0.458)e+10
+(1.831+/-0.458)e+10
+(1.880+/-0.470)e+10
+(2.124+/-0.531)e+10
+(3.472+/-0.695)e+10
+(3.531+/-0.707)e+10
+(3.549+/-0.711)e+10
+(3.546+/-0.710)e+10
+(4.233+/-0.707)e+10
+(4.089+/-0.683)e+10
+(3.731+/-0.623)e+10
+(3.314+/-0.553)e+10
+(3.409+/-0.569)e+10
+(4.200+/-0.701)e+10
+(4.250+/-0.709)e+10
+(4.169+/-0.696)e+10
+(4.915+/-0.704)e+10
+(4.936+/-0.707)e+10
+(4.941+/-0.707)e+10
+(4.829+/-0.691)e+10
+(4.928+/-0.706)e+10
+(4.872+/-0.698)e+10
+(5.508+/-0.691)e+10
+(5.561+/-0.697)e+10
+(5.586+/-0.700)e+10
+(5.445+/-0.683)e+10
+(5.369+/-0.673)e+10
+*******************
+*******************
+N mit Fehler
+9880+/-99
+9994+/-100
+10221+/-101
+10159+/-101
+10393+/-102
+10326+/-102
+10443+/-102
+10516+/-103
+10457+/-102
+10719+/-104
+13720+/-117
+16377+/-128
+16365+/-128
+15932+/-126
+14108+/-119
+10787+/-104
+10606+/-103
+10553+/-103
+10562+/-103
+10617+/-103
+10990+/-105
+12046+/-110
+13559+/-116
+13181+/-115
+10699+/-103
+10575+/-103
+10779+/-104
+10668+/-103
+10622+/-103
+10611+/-103
+10858+/-104
+10640+/-103
+10761+/-104
+10878+/-104
+10774+/-104
+10727+/-104
+11005+/-105
+11160+/-106
+******************
+'''
